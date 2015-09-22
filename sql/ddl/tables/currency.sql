90ea578b42a5f3f86dc30772af3d96c25bc136ed
Pavel Ski <pavelski01@gmail.com>
2015-09-22 21:30:54 +0200

    Update currency.sql

--- sql/ddl/tables/currency.sql
+++ sql/ddl/tables/currency.sql
@@ -3,10 +3,10 @@ CREATE TABLE currency
 	id integer NOT NULL DEFAULT nextval('currency_rate_id_seq'::regclass),
 	code character(3),
 	rate double precision,
-	symbol character varying,
-	locale character varying,
-	noun character varying,
-	adjective character varying,
+	symbol character varying(10),
+	locale character varying(10),
+	noun character varying(30),
+	adjective character varying(30),
 	CONSTRAINT currency_pkey PRIMARY KEY (id),
 	CONSTRAINT currency_code_currency_key UNIQUE (code)
 );
