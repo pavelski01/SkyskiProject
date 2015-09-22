5a38bff85db4cae996f3ea31da239cd671efe37a
Pavel Ski <pavelski01@gmail.com>
2015-09-22 21:48:14 +0200

    Update merchandise.sql

--- sql/ddl/tables/merchandise.sql
+++ sql/ddl/tables/merchandise.sql
@@ -1,7 +1,7 @@
 CREATE TABLE merchandise
 (
 	id integer NOT NULL DEFAULT nextval('merchandise_id_seq'::regclass),
-	name character varying,
+	name character varying(50),
 	countable boolean,
 	amount double precision,
 	price double precision,
