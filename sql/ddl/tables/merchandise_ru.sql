0222039767c7308420104e810bcc77f6d2eb9216
Pavel Ski <pavelski01@gmail.com>
2015-09-22 21:52:10 +0200

    Update merchandise_ru.sql

--- sql/ddl/tables/merchandise_ru.sql
+++ sql/ddl/tables/merchandise_ru.sql
@@ -1,7 +1,7 @@
 CREATE TABLE merchandise_ru
 (
 	id integer NOT NULL DEFAULT nextval('merchandise_ru_id_seq'::regclass),
-	name character varying,
+	name character varying(50),
 	CONSTRAINT merchandise_ru_pkey PRIMARY KEY (id),
 	CONSTRAINT merchandise_ru_id_fkey FOREIGN KEY (id)
 		REFERENCES merchandise (id) MATCH SIMPLE
